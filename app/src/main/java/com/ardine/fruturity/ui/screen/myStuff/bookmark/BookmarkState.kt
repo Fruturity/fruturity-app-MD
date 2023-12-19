4eae91ea55fc50e6b425d7fe5c91b34fb47f9f64
Ardine Jivensen <93922356+ArdineJ@users.noreply.github.com>
2023-12-20 03:08:23 +0700

    Delete app/src/main/java/com/ardine/fruturity/ui/screen/myStuff/bookmark/BookmarkState.kt

--- app/src/main/java/com/ardine/fruturity/ui/screen/myStuff/bookmark/BookmarkState.kt
+++
@@ -1,9 +0,0 @@
-package com.ardine.fruturity.ui.screen.myStuff.bookmark
-
-import com.ardine.fruturity.data.model.FruitHistory
-
-
-data class BookmarkState(
-    val fruitmarked: List<FruitHistory>,
-    val isBookmark: Boolean,
-)
