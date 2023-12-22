6e61ede665834ec92752b80b323937cf1358399e
Bobby  Saputra <bobbysaputra514@gmail.com>
2023-12-22 14:48:39 +0700

    fix 3

--- app/src/main/java/com/ardine/fruturity/di/Injection.kt
+++ app/src/main/java/com/ardine/fruturity/di/Injection.kt
@@ -1,8 +1,8 @@
 package com.ardine.fruturity.di
 
-import com.ardine.fruturity.data.repositories.Repository
 import com.ardine.fruturity.data.api.ApiConfig
 import com.ardine.fruturity.data.api2.ApiConfig2
+import com.ardine.fruturity.data.repositories.Repository
 
 object Injection {
     fun provideRepository(): Repository {
--- app/src/main/java/com/ardine/fruturity/ui/screen/history/HistoryViewModel.kt
+++ app/src/main/java/com/ardine/fruturity/ui/screen/history/HistoryViewModel.kt
@@ -4,8 +4,8 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.ardine.fruturity.data.repositories.Repository
 import com.ardine.fruturity.data.ResultState
+import com.ardine.fruturity.data.repositories.Repository
 import com.ardine.fruturity.data.response.FruitResponse
 import com.ardine.fruturity.ui.screen.SearchState
 import kotlinx.coroutines.flow.MutableStateFlow
